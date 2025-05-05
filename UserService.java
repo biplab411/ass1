244eb38a061e46b35967fec456b4fd0fef20b637
biplab411 <89663275+biplab411@users.noreply.github.com>
2025-05-05 12:31:19 +0530

    Create UserService.java

---
+++ UserService.java
@@ -0,0 +1,38 @@
+package com.example.service;
+
+import com.example.dto.UserDto;
+import com.example.model.User;
+import com.example.repository.UserRepository;
+import com.example.security.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    public User register(UserDto userDto) {
+        User user = new User();
+        user.setUsername(userDto.getUsername());
+        user.setPassword(passwordEncoder.encode(userDto.getPassword()));
+        user.setRole(userDto.getRole());
+        return userRepository.save(user);
+    }
+
+    public String login(String username, String password) {
+        User user = userRepository.findByUsername(username);
+        if (user != null && passwordEncoder.matches(password, user.getPassword())) {
+            return jwtTokenProvider.generateToken(user.getUsername(), user.getRole());
+        }
+        return null;
+    }
+}
